7d1b4e6f89b330f83aa0fa91fbc7161131da6a4f
JOEL ZAMORA CHAVES <joel.zamora.chaves@est.una.ac.cr>
2022-04-22 16:26:33 -0600

    b

--- app/src/main/java/com/example/lab3extra_lifecycle/MainActivity.kt
+++ app/src/main/java/com/example/lab3extra_lifecycle/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.lab3extra_lifecycle
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 
@@ -12,6 +13,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Toast.makeText(this, "onCreate() 1ra Actividad", Toast.LENGTH_SHORT).show()
+        Log.d("act", "onCreate() 1da Actividad")
+
         findViewById<Button>(R.id.btnActivity1).setOnClickListener{
             startActivity(Intent(this, SecondActivity::class.java))
         }
@@ -20,25 +23,30 @@ class MainActivity : AppCompatActivity() {
     override fun onStart(){
         super.onStart()
         Toast.makeText(this,"onStart() 1ra Actividad", Toast.LENGTH_SHORT).show()
+        Log.d("act", "onStart() 1da Actividad")
     }
 
     override fun onResume(){
         super.onResume()
         Toast.makeText(this, "onResume() 1ra Actividad", Toast.LENGTH_SHORT).show()
+        Log.d("act", "onResume() 1da Actividad")
     }
 
     override fun onPause(){
         super.onPause()
         Toast.makeText(this, "onPause() 1ra Actividad", Toast.LENGTH_SHORT).show()
+        Log.d("act", "onPause() 1da Actividad")
     }
 
     override fun onStop(){
         super.onStop()
         Toast.makeText(this, "onStop() 1ra Actividad", Toast.LENGTH_SHORT).show()
+        Log.d("act", "onStop() 1da Actividad")
     }
 
     override fun onDestroy(){
         super.onDestroy()
         Toast.makeText(this, "onDestroy() 1ra Actividad", Toast.LENGTH_SHORT).show()
+        Log.d("act", "onDestroy() 1da Actividad")
     }
 }
--- app/src/main/java/com/example/lab3extra_lifecycle/SecondActivity.kt
+++ app/src/main/java/com/example/lab3extra_lifecycle/SecondActivity.kt
@@ -3,6 +3,7 @@ package com.example.lab3extra_lifecycle
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 
@@ -12,6 +13,8 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
         Toast.makeText(this, "onCreate() 2da Actividad", Toast.LENGTH_SHORT).show()
+        Log.d("1", "onCreate() 2da Actividad")
+
         findViewById<Button>(R.id.btnActivity2).setOnClickListener{
             startActivity(Intent(this, MainActivity::class.java))
         }
@@ -20,25 +23,30 @@ class SecondActivity : AppCompatActivity() {
     override fun onStart(){
         super.onStart()
         Toast.makeText(this,"onStart() 2da Actividad", Toast.LENGTH_SHORT).show()
+        Log.d("1", "onStart() 2da Actividad")
     }
 
     override fun onResume(){
         super.onResume()
         Toast.makeText(this, "onResume() 2da Actividad", Toast.LENGTH_SHORT).show()
+        Log.d("1", "onResume() 2da Actividad")
     }
 
     override fun onPause(){
         super.onPause()
         Toast.makeText(this, "onPause() 2da Actividad", Toast.LENGTH_SHORT).show()
+        Log.d("1", "onPause() 2da Actividad")
     }
 
     override fun onStop(){
         super.onStop()
         Toast.makeText(this, "onStop() 2da Actividad", Toast.LENGTH_SHORT).show()
+        Log.d("1", "onStop() 2da Actividad")
     }
 
     override fun onDestroy(){
         super.onDestroy()
         Toast.makeText(this, "onDestroy() 2da Actividad", Toast.LENGTH_SHORT).show()
+        Log.d("1", "onDestroy() 2da Actividad")
     }
 }
